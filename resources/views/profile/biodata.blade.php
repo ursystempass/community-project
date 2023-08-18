8ea412333c2cfb3dc03438f5810fea06dd37484e
keyyy17 <keyzakhalishmurad@gmail.com>
2023-08-18 15:11:49 +0700

    menambahkan profil

---
+++ resources/views/profile/biodata.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+th, td{
+  font-family: "Cursive";
+}
+</style>
+</head>
+<body>
+
+<h2 align="center" ><b>BIODATA KOMUNITAS</b></h2>
+<p align="center">
+  <img align="center" src="bikers-subuhan4119.jpg" width="500" height="250" >
+</p>
+
+<table style="width:100%" border="1px">
+  <tr>
+
+    <th>NAMA KOMUNITAS</th>
+    <th>:</td>
+    <td>Wayang Riders</td>
+  </tr>
+  <tr>
+    <th>TANGGAL DI DIRIKAN</th>
+    <th>:</td>
+    <td>12 Februari 2000</td>
+  </tr>
+  <tr>
+    <th>ALAMAT</th>
+    <th>:</td>
+    <td>Jl.Kolonel Masturi</td>
+  </tr>
+  <tr>
+    <th>NO</th>
+    <th>:</th>
+    <td>No 12</td>
+  </tr>
+  <tr>
+    <th>RT/RW</th>
+    <th>:</td>
+    <td>03/15</td>
+  </tr>
+  <tr>
+    <th>KELURAHAN</th>
+    <th>:</td>
+    <td>Kelurahan Citeureup</td>
+  </tr>
+  <tr>
+    <th>KECAMATAN</th>
+    <th>:</td>
+    <td>Cimahi Utara</td>
+  </tr>
+  <tr>
+    <th>KOTA</th>
+    <th>:</td>
+    <td>Kota Cimahi</td>
+  </tr>
+  <tr>
+    <th>PROVINSI</th>
+    <th>:</td>
+    <td>Jawa Barat</td>
+  </tr>
+  <tr>
+    <th>NEGARA</th>
+    <th>:</td>
+    <td>INDONESIA</td>
+  </tr>
+  <tr>
+    <th>KODE POS</th>
+    <th>:</td>
+    <td>40511</td>
+  </tr>
+  <tr>
+    <th>KETUA UMUM</th>
+    <th>:</td>
+    <td>Cecep Hendrawan</td>
+  </tr>
+  <tr>
+    <th>NO TELEPON</th>
+    <th>:</td>
+    <td>O82174221767</td>
+  </tr>
+  <tr>
+    <th>EMAIL</th>
+    <th>:</td>
+    <td>wayangriders@gmail.com</td>
+  </tr>
+  <tr>
+    <th>FACEBOOK</th>
+    <th>:</td>
+    <td>Wayang Riders Komunitas</td>
+  </tr>
+  <tr>
+    <th>INSTAGRAM</th>
+    <th>:</td>
+    <td>@wayangriders__</td>
+  </tr>
+  <tr>
+    <th>WEBSITE</th>
+    <th>:</td>
+    <td>www.wayangriders.com</td>
+</table>
+</body>
+</html>
---
+++ resources/views/profile/struktur.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+  border-collapse: collapse;
+}
+</style>
+</head>
+<body>
+<h2 align="center">STRUKTUR KOMUNITAS</h2>
+<p align="center">
+    <img align="center" src="lagi (1).jpg" width="500" height="250" >
+</p>
+<table style="width:100%">
+  <tr>
+
+    <th>NAMA PENDIRI</th>
+    <th>:</th>
+    <td>RICKY DARMAWAN NOYA</td>
+  </tr>
+  <tr>
+    <th>NAMA KETUA UMUM</th>
+    <th>:</th>
+    <td>CECEP HENDRAWAN</td>
+  </tr>
+  <tr>
+    <th>WAKIL KETUA UMUM 1</th>
+    <th>:</th>
+    <td>AHMAD HUSAIN</td>
+  </tr>
+  <tr>
+    <th>WAKIL KETUA UMUM 2</th>
+    <th>:</th>
+    <td>DANI AMINATU</td>
+  </tr>
+  <tr>
+    <th>KETUA DOMISILI PROVINSI ACEH</th>
+    <th>:</th>
+    <td>SYARIF LIMANA</td>
+  </tr>
+  <tr>
+    <th>KETUA DOMISILI PROVINSI SUMATERA UTARA</th>
+    <th>:</th>
+    <td>YUDI AMBARITA</td>
+  </tr>
+  <tr>
+    <th>KETUA DOMISILI PROVINSI SUMATERA SELATAN</th>
+    <th>:</th>
+    <td>ADAM RAFIFAN</td>
+  </tr>
+  <tr>
+    <th>KETUA DOMISILI PROVINSI SUMATERA BARAT</th>
+    <th>:</th>
+    <td>ANDHIKA REIFAN</td>
+  </tr>
+  <tr>
+    <th>KETUA DOMISILI PROVINSI BENGKULU</th>
+    <th>:</th>
+    <td>ARI RIVALDI</td>
+  </tr>
+  <tr>
+    <th>KETUA DOMISILI PROVINSI RIAU</th>
+    <th>:</th>
+    <td>ARIS WAHYU</td>
+  </tr>
+  <tr>
+    <th>KETUA DOMISILI PROVINSI KEPULAUAN RIAU</th>
+    <th>:</th>
+    <td>AZZAM ZULFAN</td>
+  </tr>
+  <tr>
+    <th>KETUA DOMISILI PROVINSI JAMBI</th>
+    <th>:</th>
+    <td>DAFFA IHSAN</td>
+  </tr>
+  <tr>
+    <th>BADAN BISNIS</th>
+    <th>:</th>
+    <td>RAJIV ALVIAN</td>
+  </tr>
+  <tr>
+    <th>BADAN PROGJA</th>
+    <th>:</th>
+    <td>HADI RAHMADI</td>
+  </tr>
+  <tr>
+    <th>BADAN KEUANGAN</th>
+    <th>:</th>
+    <td>NINING SETYANINGSIH</td>
+  </tr>
+  <tr>
+    <th>BADAN SEKRETARIS</th>
+    <th>:</th>
+    <td>ARIL </td>
+  </tr>
+  <tr>
+    <th>BADAN HUMAS</th>
+    <th>:</th>
+    <td>EDI RAHMADI</td>
+  </tr>
+</table>
+</body>
+</html>
