71efa70309d9fc424a79afc87b5ad8b9ee5e58f2
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2023-08-18 14:16:18 +0700

    menambahkan routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SessionController;
+use Illuminate\Http\Request;
+use App\Models\Message;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get('/sesi', [SessionController::class, 'index'])->name('login');
+
+Route::get('/sesi/register', [SessionController::class, 'register']);
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +37,22 @@ use App\Http\Controllers\SessionController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingpage.landing'); // Mengganti 'welcome' dengan 'landingpage.landing'
 });
 
-Route::get('/sesi', [SessionController::class, 'index'])->name('login');
+Route::post('/submit-message', function (Request $request) {
+    $message = new Message();
 
-Route::get('/sesi/register', [SessionController::class, 'register']);
+    $message->nama = $request->input('nama');
+    $message->email = $request->input('email');
+    $message->no_tlp = $request->input('no_tlp');
+    $message->subject = $request->input('subject');
+    $message->pesan = $request->input('pesan');
+
+    $message->save();
+
+    return redirect('/')->with('success', 'Pesan berhasil dikirim!');
+});
+
+
+// ends
