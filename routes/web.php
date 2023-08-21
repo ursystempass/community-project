860dd3da5d8a75cbcec5c0763a37fccf73f3d723
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2023-08-21 10:10:34 +0700

    menambahkan route baru coment post dan route event

--- routes/web.php
+++ routes/web.php
@@ -42,10 +42,24 @@ Route::post('/submit-message', function (Request $request) {
     return redirect('/')->with('success', 'Pesan berhasil dikirim!');
 });
 
-Route::resource('events', EventController::class);
+Route::get('events', [EventController::class, 'index'])->name('events.index');
+Route::get('events/create', [EventController::class, 'create'])->name('events.create');
+Route::post('events', [EventController::class, 'store'])->name('events.store');
+Route::get('events/{event}', [EventController::class, 'show'])->name('events.show');
+Route::get('events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
+Route::put('events/{event}', [EventController::class, 'update'])->name('events.update');
+Route::delete('events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
+Route::get('event', function () {
+    return view('events.event');
+});
 // ends
 
 // sejarah
 Route::get('/profile/sejarah', function () {
     return view('profile.sejarah');
 })->name('sejarah');
+// end
+
+// comment post
+Route::resource('comment_posts', CommentPostController::class);
+// end
