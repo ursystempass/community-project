338fc871edc241b7a310cd1e5babf68d69ce83b5
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2023-08-22 11:15:12 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -33,15 +33,15 @@ Route::get('/', function () {
 Route::post('/submit-message', function (Request $request) {
     $message = new Message();
 
-    $message->nama = $request->input('name');
+    $message->name = $request->input('name');
     $message->email = $request->input('email');
-    $message->no_tlp = $request->input('phone_number');
+    $message->phone_number = $request->input('phone_number');
     $message->subject = $request->input('subject');
-    $message->pesan = $request->input('message');
+    $message->message = $request->input('message');
 
     $message->save();
 
-    return redirect('/')->with('success', 'Pesan berhasil dikirim!');
+    return redirect('/')->with('success', 'Pesan berhasil dikirim!');
 });
 
 Route::get('events', [EventController::class, 'index'])->name('events.index');
@@ -64,3 +64,14 @@ Route::get('/profile/sejarah', function () {
 
 
 Route::resource('comment_posts', CommentPostController::class);
+
+// dahsboard
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
+
+
+// comment post
+Route::get('/commentpost', function () {
+    return view('commentpost.index_users');
+});
